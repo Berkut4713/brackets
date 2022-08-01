52072d5115f11b87ad4b077d53ad37f0c47b3d72
Berkut4713 <berkut_bm@mail.ru>
2022-08-02 00:46:27 +0300

    feat: index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+/*module.exports = function check(str, bracketsConfig) {
+function check(str, bracketsConfig) {
+    let stack = [];
+  for (let i = 0; i < str.length; i++) {    
+    stack.push(str[i]);
+  }
+  return true;
+  const config3 = [['(', ')'], ['[', ']'], ['{', '}']]; 
+
+check('([{}])', config3);
+}*/
+
+
+console.log('dfaefas');
