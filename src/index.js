283e255ccea551720d7a56ad3a0aa8924d562b4f
Berkut4713 <berkut_bm@mail.ru>
2022-08-02 20:38:42 +0300

    feat: ok

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-/*module.exports = function check(str, bracketsConfig) {
-function check(str, bracketsConfig) {
-    let stack = [];
-  for (let i = 0; i < str.length; i++) {    
+module.exports = function check(str, bracketsConfig) {
+/*function check(str, bracketsConfig) {*/
+  let stack = [];
+for (let i = 0; i < str.length; i++) {    
+  if (isСouple(stack[stack.length-1],str[i],bracketsConfig)) {
+    stack.pop(str[i]);
+  } else {
     stack.push(str[i]);
   }
-  return true;
-  const config3 = [['(', ')'], ['[', ']'], ['{', '}']]; 
+  }
+  console.log(stack.length === 0);
+return stack.length === 0;
+}
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']]; 
 
-check('([{}])', config3);
-}*/
+/*check('([{}()])', config3);*/
 
-
-console.log('dfaefas');
+function isСouple(open, close, bracketsConfig) {
+for (let j = 0; j < bracketsConfig.length; j++) {
+  if (bracketsConfig[j][0] === open) {
+    if (bracketsConfig[j][1] === close) {
+      return true;
+    }
+  }    
+}
+return false;
+}
